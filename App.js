a010f3d6501ed7af1b2cb763728969091915b9a5
Lucas da Mata <lucasmsilva@unipam.edu.br>
2021-05-26 18:41:52 -0300

    Finalizando componente navigation e utilização de icones dinamicos

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import  Icon  from 'react-native-vector-icons/Ionicons'
+
+import Home from './src/pages/home';
+import Contatos from './src/pages/contatos';
+import Postagem from './src/pages/postagem';
+import Vagas from './src/pages/vagas';
+
+
+const Tab = createBottomTabNavigator();
+
+const icons = {
+  Home: {
+    name: 'ios-home'
+  },
+  Contatos: {
+    name: 'ion-ios-contacts'
+  },
+  Postagem: {
+    name: 'ion-ios-add-circle'
+  },
+  Vagas: {
+    name: 'ion-ios-briefcase'
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <Tab.Navigator
+          screenOptions= { (route) => ({
+            tabBarIcon: ({ color, size }) => {
+              const {name} = icons[route.name];
+              return <Icon name={name} color={color} size={size} />
+            }
+          }) }
+          tabBarOptions={{
+            style:{
+              backgroundColor: '#000'
+            },
+            activeBackgroundColor: '#FFF'
+          }}
+        >
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Contatos" component={Contatos} />
+            <Tab.Screen name="Publicação" component={Postagem} />
+            <Tab.Screen name="Vagas" component={Vagas} />
+        </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/pages/vagas/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Vagas() {
+  return (
+    <View>
+      <Text>Vagas</Text>
+    </View>
+  );
+}
